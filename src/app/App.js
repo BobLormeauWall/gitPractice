47dce95ee59f34531231b2e28663174e2313c940
Arthur Lormeau Wall <arthurlormeau@gmail.com>
2021-12-21 10:30:13 +0100

    my name is alice

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,11 @@
 import './App.scss';
 import '../scss/custom.scss'
 
-<<<<<<< HEAD
+
 function App() 
 
 let name='my name is alice'
 
-
-=======
-function App() {
-
-  let name='my name is bob'
-  
->>>>>>> 8fb957f99f1388e04ad1eef02442304801b7a42b
   return (
     <div className="App">
       <h1> Hello world! </h1>
